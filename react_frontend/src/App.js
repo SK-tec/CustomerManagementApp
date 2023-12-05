442ecfa29f0acf2728b3aece781ea68debd236a9
samatha <samathagamidi@gmail.com>
2023-12-05 21:56:05 +0100

    Accessing the Spring Boot REST API from  React frontend

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data))
+      .catch((error) => console.error("Error fetching customers:", error));
+  }, []);
   return (
     <div className="App">
       <h1 className="text-center text-warning">Customer Service App</h1>
+      <h2>Customer's List</h2>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.id}>
+            Name: {customer.firstName}
+            {customer.lastName} - Email: {customer.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- springboot_backend/src/main/java/com/ecorvi/springboot_backend/controller/CustomerController.java
+++ springboot_backend/src/main/java/com/ecorvi/springboot_backend/controller/CustomerController.java
@@ -1,18 +1,32 @@
 package com.ecorvi.springboot_backend.controller;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecorvi.springboot_backend.model.Customer;
 import com.ecorvi.springboot_backend.service.CustomerService;
+
+@CrossOrigin(
+    origins = {
+        "http://localhost:3000",        
+        },
+    methods = {
+                RequestMethod.OPTIONS,
+                RequestMethod.GET,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.POST
+})
+
+//@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = "*")
 
 @RestController
 @RequestMapping("/api/customers")
